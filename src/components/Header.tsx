6ebcc8fe07c79daa0c3c5311897f81524840cf06
zainkhan1726 <160050990+zainkhan1726@users.noreply.github.com>
2024-10-28 13:36:29 +0500

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return(
+        <div className=" bg-gray-500 h-[70px] ">
+            {/* <div>LOGO</div>
+            <div className="flex gap-4">
+                <div>Home</div>
+                <div>About</div>
+                <div>Services</div>
+                <div>Contact us</div>
+            </div> */}
+        </div>
+    )
+}
